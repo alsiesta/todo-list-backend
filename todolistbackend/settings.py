fb5fb7736dd6b392b791360c59573125266c1a57
alsiesta <as@daslizenzatelier.de>
2024-04-24 23:49:46 +0200

    created login api

--- todolistbackend/settings.py
+++ todolistbackend/settings.py
@@ -25,8 +25,10 @@ SECRET_KEY = 'django-insecure-2q6%i2l-orsq#r0kpp6d+%-8gxszon--5vq1ajv#n5&6dzswjz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost:4200','127.0.0.1']
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200/']
+# CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
