7a198b5e8f3f64e3fe503c15529c2adb11a1dbac
alsiesta <as@daslizenzatelier.de>
2024-04-25 14:07:02 +0200

    created ToDo Model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+        )
+    created_at = models.DateTimeField(default=timezone.now)
+    checked = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+                                      
+                                      
